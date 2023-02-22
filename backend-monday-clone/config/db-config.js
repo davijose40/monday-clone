54cb70de414d3f1ddaf8853ea10b478e61803c36
Davi Jose <davijose40@gmail.com>
2023-02-22 17:39:56 -0300

    delete config keys
    db-config bring secret key

--- backend-monday-clone/config/db-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    PORT:process.env.PORT,
-    DB_URL_COLLECTION:process.env.DB_URL_COLLECTION,
-    DB_URL:process.env.DB_URL_DOCUMENT,
-    TOKEN:process.env.DB_TOKEN
-})
